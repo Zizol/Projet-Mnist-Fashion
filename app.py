d6d67963613709b1188f65738890603a9ea6d7e3
Zizol <adrien.videgrain@gmail.com>
2020-02-06 18:40:24 +0100

    Fichier avec structure du travail à effectuer

---
+++ app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+
+# Données d'entrainement et labels
+## Importer datasets
+
+
+## Afficher quelques images
+
+
+
+# Données de validation
+
+
+
+# Définition du modèle
+
+
+# Loss et optimisation
+
+
+# Metriques
+
+
+# Entrainement
+## Training
+
+
+## Testing
+
+
+# Affichage attendu
+
+
+# Affichage valeurs de test
+
+
+# Performances
+
